86972b7f3745c7a6fd59d869f72426f03d4ca5f5
komal-tiwari-dev <atulzv1999@gmail.com>
2022-06-08 00:49:38 +0530

    Figma complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "./contexxt/ThemeContext";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
